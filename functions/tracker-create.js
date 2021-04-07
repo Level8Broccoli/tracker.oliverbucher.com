3e3eee2197b1942d638a0b0b8a12d9fa0d6c11f9
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-04-07 21:33:50 +0000

    it works?

--- functions/tracker-create.js
+++ functions/tracker-create.js
@@ -1,14 +1,13 @@
 import faunadb, { query } from 'faunadb';
 const { Create, Collection } = query;
 
-const FAUNADB_SECRET = 'fnAEGOUMcZACB3ni6zD_KxazWyeuiBksZQR3IYV8';
-
 const client = new faunadb.Client({
-    secret: process.env.FAUNADB_SECRET || FAUNADB_SECRET
+    secret: process.env.FAUNADB_SECRET
 });
 
-console.log(process.env.FAUNADB_SECRET);
+
 export async function handler(event) {
+    console.log({ secret: process.env.FAUNADB_SECRET });
     try {
         const data = JSON.parse(event.body);
         const res = await client.query(Create(Collection('tracks'), { data }));
