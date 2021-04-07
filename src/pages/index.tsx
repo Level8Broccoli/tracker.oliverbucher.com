2d95cdeee332bfdf62bfc5158cd03ea30bd83895
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-04-07 19:26:21 +0000

    remove functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,5 @@
 import Head from 'next/head';
 
-// Function using fetch to POST to our API endpoint
-function createTrack(data: any) {
-    return fetch('/.netlify/functions/track-create', {
-        body: JSON.stringify(data),
-        method: 'POST'
-    }).then((response) => {
-        return response.json();
-    });
-}
-
-// Todo data
-const myTrack = {
-    title: 'My todo title',
-    completed: false
-};
-
-// create it!
-createTrack(myTrack)
-    .then((response) => {
-        console.log('API response', response);
-        // set app state
-    })
-    .catch((error) => {
-        console.log('API error', error);
-    });
-
 export default function Home(): JSX.Element {
     return (
         <div>
