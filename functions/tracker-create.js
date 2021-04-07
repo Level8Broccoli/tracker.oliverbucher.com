35a8631f84777bf5f07f220c740af73bf7667f28
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-04-07 22:21:05 +0000

    update

--- functions/tracker-create.js
+++ functions/tracker-create.js
@@ -6,13 +6,22 @@ const client = new faunadb.Client({
 });
 
 export async function handler(event) {
+    const getHeaders = () => {
+        if (process.env.LOCAL) {
+            return {
+                'access-control-allow-origin': '*'
+            };
+        }
+        return undefined;
+    };
     try {
         const data = JSON.parse(event.body);
         const res = await client.query(Create(Collection('tracks'), { data }));
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ res, method: event.httpMethod })
+            body: JSON.stringify({ res, method: event.httpMethod }),
+            headers: getHeaders()
         };
     } catch (e) {
         return {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 
 export default function Home(): JSX.Element {
     const [url, setUrl] = useState('');
     const [host, setHost] = useState('');
 
     useEffect(() => {
-        const HOST = typeof window === 'undefined' ? 'localhost' : window.location.host;
-        const ENDPOINT = '/.netlify/functions/tracker-create';
-        setUrl(HOST === 'localhost' ? `http://localhost:9000${ENDPOINT}` : ENDPOINT);
+        const HOST =
+            typeof window === 'undefined' || window.location.hostname === 'localhost'
+                ? 'localhost'
+                : window.location.href;
+        const ENDPOINT = '.netlify/functions/tracker-create';
+        setUrl(HOST === 'localhost' ? `http://localhost:9000/${ENDPOINT}` : HOST + ENDPOINT);
         setHost(HOST);
     }, []);
+
+    const createTracker = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const res = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({ name: 'test' })
+        });
+        await console.log({ res, url, host });
+    };
 
     return (
         <div>
@@ -21,16 +33,11 @@ export default function Home(): JSX.Element {
 
             <main>
                 <h1>Erstelle deinen eigenen Tracker</h1>
-                <form action={url} method="POST">
-                    <div className="field">
-                        <label className="label" htmlFor="name">
-                            Name
-                        </label>
-                        <div className="control">
-                            {host}/
-                            <input className="input" type="text" placeholder="Text input" />
-                        </div>
-                    </div>
+                <form onSubmit={createTracker}>
+                    <label htmlFor="slug">Gib deinem Tracker einen Namen:</label>
+                    {host}/
+                    <input id="slug" type="text" placeholder="dein-name" />
+                    <button type="submit">Erstellen</button>
                 </form>
             </main>
         </div>
