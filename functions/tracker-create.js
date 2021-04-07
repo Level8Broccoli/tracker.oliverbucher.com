08b3988133948cfbcc06d2183034fafef8b73677
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-04-07 21:52:35 +0000

    update interface

--- functions/tracker-create.js
+++ functions/tracker-create.js
@@ -5,9 +5,7 @@ const client = new faunadb.Client({
     secret: process.env.FAUNADB_SECRET
 });
 
-
 export async function handler(event) {
-    console.log({ secret: process.env.FAUNADB_SECRET });
     try {
         const data = JSON.parse(event.body);
         const res = await client.query(Create(Collection('tracks'), { data }));
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 export default function Home(): JSX.Element {
+    const [url, setUrl] = useState('');
+    const [host, setHost] = useState('');
+
+    useEffect(() => {
+        const HOST = typeof window === 'undefined' ? 'localhost' : window.location.host;
+        const ENDPOINT = '/.netlify/functions/tracker-create';
+        setUrl(HOST === 'localhost' ? `http://localhost:9000${ENDPOINT}` : ENDPOINT);
+        setHost(HOST);
+    }, []);
+
     return (
         <div>
             <Head>
@@ -8,7 +19,20 @@ export default function Home(): JSX.Element {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main>tracker.oliverbucher.com</main>
+            <main>
+                <h1>Erstelle deinen eigenen Tracker</h1>
+                <form action={url} method="POST">
+                    <div className="field">
+                        <label className="label" htmlFor="name">
+                            Name
+                        </label>
+                        <div className="control">
+                            {host}/
+                            <input className="input" type="text" placeholder="Text input" />
+                        </div>
+                    </div>
+                </form>
+            </main>
         </div>
     );
 }
