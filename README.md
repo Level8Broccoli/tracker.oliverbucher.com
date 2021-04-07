3af665cde9242feca36dccc85cec85d7e55a1efb
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-04-07 16:32:37 +0000

    init next

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # [tracker.oliverbucher.com](tracker.oliverbucher.com)
+
+***node_modules folder is placed in the root directory and linked via soft link!***
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Tracker</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>tracker.oliverbucher.com</main>
+    </div>
+  );
+}
