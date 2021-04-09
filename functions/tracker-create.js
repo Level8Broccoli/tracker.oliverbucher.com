9345dc568be8105ab8f0ae9d4f3b955cfab04385
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-04-09 13:15:36 +0000

    refactorings

--- functions/tracker-create.js
+++ functions/tracker-create.js
@@ -1,30 +1,14 @@
-import { Client, Collection, CreateCollection, Exists } from 'faunadb';
-import { getHeaders, returnError, validateTrackName } from './util/utils';
-import { COLLECTION_NAME_CONFIG } from './util/config';
-
-const db = new Client({
-    secret: process.env.FAUNADB_SECRET
-});
-
-const createCollection = async (name) => {
-    await db.query(CreateCollection({ name }));
-};
+import {
+    getHeaders,
+    returnError,
+    validateTrackName,
+    createConfigCollectionIfNotExists
+} from './util/utils';
 
 export async function handler({ body }) {
     try {
         validateTrackName(JSON.parse(body));
-
-        const collectionExists = await db.query(Exists(Collection(COLLECTION_NAME_CONFIG)));
-        if (!collectionExists) {
-            console.log(`Collection "${COLLECTION_NAME_CONFIG}" does not yet exists. Creating...`);
-            try {
-                await createCollection(COLLECTION_NAME_CONFIG);
-                console.log(`Collection "${COLLECTION_NAME_CONFIG}" created.`);
-            } catch (e) {
-                console.log(`Error while creating collection "${COLLECTION_NAME_CONFIG}"!`, e);
-                throw e;
-            }
-        }
+        createConfigCollectionIfNotExists();
 
         return {
             statusCode: 200,
--- functions/util/utils.js
+++ functions/util/utils.js
@@ -1,4 +1,9 @@
 import { COLLECTION_NAME_CONFIG } from './config';
+import { Client, Collection, CreateCollection, Exists } from 'faunadb';
+
+const db = new Client({
+    secret: process.env.FAUNADB_SECRET
+});
 
 export const getHeaders = () => {
     if (process.env.LOCAL) {
@@ -31,4 +36,22 @@ export const validateTrackName = (body) => {
     if (!regexRule.test(trackName)) {
         throw `Invalid trackName '${trackName}'`;
     }
+};
+
+export const createConfigCollectionIfNotExists = async () => {
+    const collectionExists = await db.query(Exists(Collection(COLLECTION_NAME_CONFIG)));
+    if (!collectionExists) {
+        console.log(`Collection "${COLLECTION_NAME_CONFIG}" does not yet exists. Creating...`);
+        try {
+            await createCollection(COLLECTION_NAME_CONFIG);
+            console.log(`Collection "${COLLECTION_NAME_CONFIG}" created.`);
+        } catch (e) {
+            console.log(`Error while creating collection "${COLLECTION_NAME_CONFIG}"!`, e);
+            throw e;
+        }
+    }
+};
+
+export const createCollection = async (name) => {
+    await db.query(CreateCollection({ name }));
 };
