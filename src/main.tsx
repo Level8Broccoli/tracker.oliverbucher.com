36ed5e91647abee069cc034bb90bbb2f25646b4a
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-04-11 10:59:42 +0000

    add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './styles/globals.scss';
 import Home from './pages/Home';
+import Tracker from './pages/Tracker';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Home />
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/:name">
+                    <Tracker />
+                </Route>
+                <Route path="*">
+                    <h1>404</h1>
+                </Route>
+            </Switch>
+        </Router>
     </React.StrictMode>,
     document.getElementById('root')
 );
--- src/pages/Tracker.tsx
+++ src/pages/Tracker.tsx
@@ -1,29 +1,29 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import { createEntryRequest, readAllEntryRequest } from '../api/api';
 import { entry } from '../api/schemas';
 import { getSecret } from '../utils/storage';
 
 export default function Tracker(): JSX.Element {
-    // const router = useRouter();
-    // const { slug: name } = router.query;
+    const { name } = useParams<{ name: string }>();
     const [entries, setEntries] = useState<entry[]>([]);
 
-    // useEffect(() => {
-    //     (async () => {
-    //         if (typeof name === 'string') {
-    //             const data = await readAllEntryRequest(name);
-    //             console.log({ data });
-    //             setEntries(data);
-    //         }
-    //     })();
-    // }, [name]);
+    useEffect(() => {
+        (async () => {
+            if (typeof name === 'string') {
+                const data = await readAllEntryRequest(name);
+                console.log({ data });
+                setEntries(data);
+            }
+        })();
+    }, [name]);
 
     const createEntry = () => {
         const secret = getSecret();
         if (typeof secret === 'string' && typeof name === 'string') {
             console.log(secret);
 
-            // createEntryRequest(name, secret);
+            createEntryRequest(name, secret);
         }
     };
 
