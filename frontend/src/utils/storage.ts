00918b9105a1c8c072d51d3ba61dae42b5583e32
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-04-16 20:24:25 +0000

    improve secret storage

--- frontend/src/utils/storage.ts
+++ frontend/src/utils/storage.ts
@@ -1,11 +1,11 @@
 export const getSecret = (name: string): string | null => {
-    return window?.localStorage?.getItem(name);
+    return window.localStorage.getItem(name.toLocaleLowerCase());
 };
 
 export const saveSecret = (name: string, secret: string): void => {
-    window.localStorage.setItem(name, secret);
+    window.localStorage.setItem(name.toLocaleLowerCase(), secret);
 };
 
 export const deleteSecret = (name: string): void => {
-    window.localStorage.removeItem(name);
+    window.localStorage.removeItem(name.toLocaleLowerCase());
 };
