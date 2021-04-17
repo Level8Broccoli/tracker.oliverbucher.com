8212828f0f83680832f3b33300a820d7b31c0c49
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-04-17 14:00:09 +0000

    update

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,17 +1,13 @@
-import React, { FormEvent, useEffect, useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router';
 import { trackerCreate } from '../api/trackerCreate';
 import { NAME_RULE } from '../config';
+import Stack from '../layout/Stack';
 import { saveSecret } from '../utils/storage';
 
 export default function Home(): JSX.Element {
     const [name, setName] = useState('');
-    const [host, setHost] = useState('');
     const history = useHistory();
-
-    useEffect(() => {
-        setHost(`https://${window.location.host}/`);
-    }, []);
 
     const submitNewTracker = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -28,20 +24,37 @@ export default function Home(): JSX.Element {
 
     return (
         <section>
-            <h2>Erstelle deinen eigenen Tracker</h2>
-            <form onSubmit={submitNewTracker}>
-                <label htmlFor="name">Gib deinem Tracker einen Namen:</label>
-                {host}
-                <input
-                    id="name"
-                    type="text"
-                    placeholder="dein-name"
-                    pattern={NAME_RULE.toString().slice(1, -1)}
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <button type="submit">Erstellen</button>
-            </form>
+            <Stack>
+                <h2>Erstelle deinen eigenen Tracker</h2>
+                <form onSubmit={submitNewTracker}>
+                    <div className="addon">
+                        <label htmlFor="name" className="prefix">
+                            trkr.lvl8.io/
+                        </label>
+                        <input
+                            id="name"
+                            type="text"
+                            placeholder="dein-name"
+                            pattern={NAME_RULE.toString().slice(1, -1)}
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <button type="submit">
+                            <i className="fad fa-file-plus"></i>
+                        </button>
+                    </div>
+                </form>
+                <div>
+                    <p className="small-print">Dein Name darf</p>
+                    <ul className="small-print">
+                        <li>(A-Z): Grossbuchstaben,</li>
+                        <li>(a-z): Kleinbuchstaben,</li>
+                        <li>(0-9): Zahlen und</li>
+                        <li>(-): Trennstriche</li>
+                    </ul>
+                    <p className="small-print">beinhalten.</p>
+                </div>
+            </Stack>
         </section>
     );
 }
