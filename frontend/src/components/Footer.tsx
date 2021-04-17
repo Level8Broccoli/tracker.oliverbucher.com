fcb16858d6afc3b17bd147d1ce56e1b34b5468c5
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-04-17 11:06:18 +0000

    Kontakt in the footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function Footer(): JSX.Element {
-    return <footer>Footer</footer>;
+    return (
+        <footer>
+            <a href="https://kontakt.trkr.lvl8.io/">Kontakt</a>
+        </footer>
+    );
 }
