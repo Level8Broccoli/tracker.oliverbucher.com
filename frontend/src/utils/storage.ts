89d3361f6a1e93521922094def4cbe4c0ab6a42a
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-04-17 14:47:07 +0000

    change storage

--- frontend/src/utils/storage.ts
+++ frontend/src/utils/storage.ts
@@ -1,11 +1,35 @@
 export const getSecret = (name: string): string | null => {
-    return window.localStorage.getItem(name.toLocaleLowerCase());
+    const obj = window.localStorage.getItem(name.toLocaleLowerCase());
+    if (obj === null) {
+        return null;
+    }
+    try {
+        const { secret } = JSON.parse(obj);
+        return secret;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 };
 
-export const saveSecret = (name: string, secret: string): void => {
-    window.localStorage.setItem(name.toLocaleLowerCase(), secret);
+export const saveSecret = (name: string, secret: string, first = false): void => {
+    window.localStorage.setItem(name.toLocaleLowerCase(), JSON.stringify({ secret, first }));
 };
 
 export const deleteSecret = (name: string): void => {
     window.localStorage.removeItem(name.toLocaleLowerCase());
+};
+
+export const isFirst = (name: string): boolean => {
+    const obj = window.localStorage.getItem(name.toLocaleLowerCase());
+    if (obj === null) {
+        return false;
+    }
+    try {
+        const { first } = JSON.parse(obj);
+        return first;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
 };
